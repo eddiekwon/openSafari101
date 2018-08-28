c13338ac616e66d9d23b9e864cf19d00ee080a30
eddiekwon <hallomuze@gmail.com>
2018-08-28 12:59:42 +0900

    Initial Commit

---
+++ OpenSafari101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenSafari101
+//
+//  Created by eddiek on 28/08/2018.
+//  Copyright © 2018 Eddie Kwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
