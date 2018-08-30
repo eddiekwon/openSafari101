25a27df8572bc2b01174d6eab904aea8f5109426
eddiekwon <eddiekwon7@gmail.com>
2018-08-30 17:39:18 +0900

    init

--- OpenSafari101/ViewController.swift
+++ OpenSafari101/ViewController.swift
@@ -7,19 +7,77 @@
 //
 
 import UIKit
+import SafariServices
+import WebKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // 1 launch safari then going to web page
+    @IBAction func openSafari(_ sender: Any) {
+        guard let url = URL(string: "http://www.stackoverflow.com") else { return }
+        if #available(iOS 10.0 , *) {
+            UIApplication.shared.open(url)
+        }else{
+            UIApplication.shared.openURL(url)
+        }
     }
+    
+    func openYoutube(){
+        guard let url = URL(string: "https://youtu.be/9bZkp7q19f0") else { return }
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    // 2 open webpage inside App.
+    @IBAction func showSafariHere(_ sender: Any) {
+         guard let url = URL(string: "http://www.stackoverflow.com") else { return }
+        let svc = SFSafariViewController(url: url)
+        present(svc , animated: true , completion: nil)
+    }
+    
+    //3 open webpage, but cannot go back.
+    @IBAction func useWKWebview(_ sender: Any) {
+        loadWkWebview()
+    }
+    
+    let webView = WKWebView()
+    
+}
 
-
+extension ViewController:  WKNavigationDelegate{
+    func loadWkWebview() {
+        
+        guard let url = URL(string: "https://www.google.com") else { return }
+        webView.frame = view.bounds
+        webView.navigationDelegate = self
+        webView.load(URLRequest(url: url))
+        webView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        view.addSubview(webView)
+    }
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.navigationType == .linkActivated  {
+            if let url = navigationAction.request.url,
+                let host = url.host, !host.hasPrefix("www.google.com"),
+                UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+                print(url)
+                print("Redirected to browser. No need to open it locally")
+                decisionHandler(.cancel)
+            } else {
+                print("Open it locally")
+                decisionHandler(.allow)
+            }
+        } else {
+            print("not a user click")
+            decisionHandler(.allow)
+        }
+    }
 }
 
